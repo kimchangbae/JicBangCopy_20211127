15c35232edc0adfc40aa95dd476d1fbc754b443f
김창배 <cbkim93@naver.com>
2021-11-27 15:50:26 +0900

    Use Handler class - after 2.5sec move MainActivity

--- app/src/main/java/com/example/jicbangcopy_20211127/SplashActivity.kt
+++ app/src/main/java/com/example/jicbangcopy_20211127/SplashActivity.kt
@@ -1,11 +1,23 @@
 package com.example.jicbangcopy_20211127
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.os.postDelayed
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val myHandler = Handler( Looper.getMainLooper() ) // Main Thread Call
+        myHandler.postDelayed({
+            // after 2.5sec todo move mainActivity
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        },2500)
     }
 }
